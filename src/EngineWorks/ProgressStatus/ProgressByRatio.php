1c9e14a6dcebaa8226eac209ba617ab777f3cdd5
Carlos C Soto <eclipxe13@gmail.com>
2017-02-09 12:30:17 -0600

    Fix ProgressByRatio logic

--- src/EngineWorks/ProgressStatus/ProgressByRatio.php
+++ src/EngineWorks/ProgressStatus/ProgressByRatio.php
@@ -17,15 +17,59 @@ class ProgressByRatio extends Progress
      * @param float $ratio
      * @param int $precision
      */
-    public function __construct(Status $status, array $observers = [], $ratio = 0.01, $precision = 2)
+    public function __construct(Status $status = null, array $observers = [], $ratio = 0.01, $precision = 2)
     {
         parent::__construct($status, $observers);
-        $this->ratio = $ratio;
-        $this->precision = $precision;
+        $this->setPrecision($precision);
+        $this->setRatio($ratio);
     }
 
-    public function shouldNotifyChange(Status $currentStatus, Status $newstatus)
+    public function shouldNotifyChange(Status $currentStatus, Status $newStatus)
     {
-        return round($currentStatus->getRatio() - $newstatus->getRatio() - $this->ratio, $this->precision) >= 0.0;
+        $current = (int) ceil(round($currentStatus->getRatio(), $this->precision) / $this->ratio);
+        $new = (int) ceil(round($newStatus->getRatio(), $this->precision) / $this->ratio);
+        return ($current !== $new);
+    }
+
+    /**
+     * @return float
+     */
+    public function getRatio()
+    {
+        return $this->ratio;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrecision()
+    {
+        return $this->precision;
+    }
+
+    /**
+     * @param float $ratio
+     */
+    protected function setRatio($ratio)
+    {
+        if (! is_int($ratio) && ! is_float($ratio)) {
+            throw new \InvalidArgumentException('Ratio is not a float value');
+        }
+        $ratio = round($ratio, $this->precision);
+        if ($ratio < pow(10, - $this->precision)) {
+            throw new \InvalidArgumentException('Ratio change is lower than minimum value of precision');
+        }
+        $this->ratio = $ratio;
+    }
+
+    /**
+     * @param int $precision
+     */
+    protected function setPrecision($precision)
+    {
+        if (! is_int($precision) || 0 === $precision) {
+            throw new \InvalidArgumentException('Precision must be an positive integer greater or equals to zero');
+        }
+        $this->precision = $precision;
     }
 }
