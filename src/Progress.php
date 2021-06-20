3a3bd9e5d79ac831c69863666207e92a323759f8
Carlos C Soto <eclipxe13@gmail.com>
2021-06-20 02:51:41 -0500

    Add strict_types declaration

--- src/Progress.php
+++ src/Progress.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace EngineWorks\ProgressStatus;
 
 class Progress extends AbstractSplSubject implements ProgressInterface
--- tests/NullProgressTest.php
+++ tests/NullProgressTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace EngineWorks\ProgressStatus\Tests;
 
 use EngineWorks\ProgressStatus\NullProgress;
